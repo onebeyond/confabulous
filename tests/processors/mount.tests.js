87a31eff63f68f6cc19a5b63dc6c4e7afab3896d
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-06-04 23:42:47 +0100

    Adding test

---
+++ tests/processors/mount.tests.js
@@ -0,0 +1,11 @@
+var assert = require('chai').assert
+var mount = require('../../lib/processors/mount')
+
+describe('mount', function() {
+    it('should mount config at the specified key', function() {
+        mount({ key : 'foo.bar' })({ baz: 1 }, function(err, config) {
+            assert.ifError(err)
+            assert.equal(config.foo.bar.baz, 1)
+        })
+    })
+})
