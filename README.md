080b4be35cbc7fd5c6ea2435a044d6aacfb8b52e
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-06-23 06:47:51 +0100

    Updating readme and example

--- README.md
+++ README.md
@@ -4,28 +4,27 @@ Confabulous is a hierarchical, asynchronous config loader and post processor. It
 
 ## TL;DR
 ```
-var Confabulous = require('..').Confabulous
-var loaders = require('..').loaders
-var processors = require('..').processors
+const confabulous = require('confabulous')
+const Confabulous = confabulous.Confabulous
+const loaders = confabulous.loaders
+const processors = confabulous.processors
 
 new Confabulous()
-  .add((config) => loaders.require({ path: './conf/defaults.js', watch: true }))
-  .add((config) => loaders.require({ path: `./conf/${process.env.NODE_ENV}.js`, watch: true }))
-  .add((config) => loaders.require({ path: './conf/runtime.js', mandatory: false }))
-  .add((config) => loaders.file({ path: './conf/secret.json.encrypted' }, [
-        processors.decrypt({ algorithm: 'aes192', password: process.env.SECRET }),
+    .add((config) => loaders.env([ processors.mount({ key: 'env' }) ]))
+    .add((config) => loaders.require({ path: './conf/defaults.js', watch: true }))
+    .add((config) => loaders.require({ path: `./conf/${config.env.NODE_ENV}.js`, watch: true }))
+    .add((config) => loaders.require({ path: './conf/runtime.js', mandatory: false }))
+    .add((config) => loaders.file({ path: './conf/secret.json.encrypted' }, [
+        processors.decrypt({ algorithm: 'aes192', password: config.env.SECRET }),
         processors.json()
-  ])
-  .add((config) => loaders.args())
-  .on('loaded', (config) => {
-    console.log('Loaded', JSON.stringify(config, null, 2))
-  }).on('reloaded', (config) => {
-    console.log('Reloaded', JSON.stringify(config, null, 2))
-  }).on('error', (err) => {
-    console.error('Error', err)
-  }).on('reload_error', (err) => {
-    console.error('Reload Error', err)
-  }).end()
+    ]))
+    .add((config) => loaders.args())
+    .add((config) => loaders.http({ url: config.server.url, mandatory: false, watch: { interval: '5m' } }))
+    .on('loaded', (config) => console.log('Loaded', JSON.stringify(config, null, 2)))
+    .on('reloaded', (config) => console.log('Reloaded', JSON.stringify(config, null, 2)))
+    .on('error', (err) => console.error('Error', err))
+    .on('reload_error', (err) => console.error('Reload Error', err))
+    .end()
 ```
 
 ## Loaders
--- examples/index.js
+++ examples/index.js
@@ -1,35 +1,24 @@
-var noop = require('lodash.noop')
-var Confabulous = require('..').Confabulous
-var loaders = require('..').loaders
-var processors = require('..').processors
+const noop = require('lodash.noop')
+const confabulous = require('..')
+const Confabulous = confabulous.Confabulous
+const loaders = confabulous.loaders
+const processors = confabulous.processors
 
-new Confabulous().add((config) => {
-    return loaders.env([
-        processors.mount({ key: 'env' })
-    ])
-}).add((config) => {
-    return loaders.require({ path: './conf/defaults.js', watch: true })
-}).add((config) => {
-    return loaders.require({ path: `./conf/${config.env.NODE_ENV}.js`, watch: true })
-}).add((config) => {
-    return loaders.require({ path: './conf/runtime.js', mandatory: false })
-}).add((config) => {
-    return loaders.file({ path: './conf/secret.json.encrypted' }, [
+new Confabulous()
+    .add((config) => loaders.env([ processors.mount({ key: 'env' }) ]))
+    .add((config) => loaders.require({ path: './conf/defaults.js', watch: true }))
+    .add((config) => loaders.require({ path: `./conf/${config.env.NODE_ENV}.js`, watch: true }))
+    .add((config) => loaders.require({ path: './conf/runtime.js', mandatory: false }))
+    .add((config) => loaders.file({ path: './conf/secret.json.encrypted' }, [
         processors.decrypt({ algorithm: 'aes192', password: config.env.SECRET }),
         processors.json()
-    ])
-}).add((config) => {
-    return loaders.args()
-}).add((config) => {
-    return loaders.http({ url: config.server.url, mandatory: false, watch: { interval: '5m' } })
-}).on('loaded', (config) => {
-    console.log('Loaded', JSON.stringify(config, null, 2))
-}).on('reloaded', (config) => {
-    console.log('Reloaded', JSON.stringify(config, null, 2))
-}).on('error', (err) => {
-    console.error('Error', err)
-}).on('reload_error', (err) => {
-    console.error('Reload Error', err)
-}).end()
+    ]))
+    .add((config) => loaders.args())
+    .add((config) => loaders.http({ url: config.server.url, mandatory: false, watch: { interval: '5m' } }))
+    .on('loaded', (config) => console.log('Loaded', JSON.stringify(config, null, 2)))
+    .on('reloaded', (config) => console.log('Reloaded', JSON.stringify(config, null, 2)))
+    .on('error', (err) => console.error('Error', err))
+    .on('reload_error', (err) => console.error('Reload Error', err))
+    .end()
 
 setInterval(noop, Number.MAX_SAFE_INTEGER)
