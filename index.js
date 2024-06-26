416790f170c13c4fd45fdf5765aa41c1a2cd1748
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-06-26 17:59:35 +0200

    refactor: remove require-all and do the imports explicitly

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const requireAll = require('require-all');
 const path = require('path');
 
 const Confabulous = require('./lib/Confabulous');
 
 Confabulous.Confabulous = Confabulous; // Remain backwards compatible with old API (yuck!!!!)
-Confabulous.loaders = requireAll(path.join(__dirname, './lib/loaders'));
-Confabulous.processors = requireAll(path.join(__dirname, './lib/processors'));
+
+Confabulous.loaders = {
+  args: require('./lib/loaders/args'),
+  echo: require('./lib/loaders/echo'),
+  env: require('./lib/loaders/env'),
+  file: require('./lib/loaders/file'),
+  require: require('./lib/loaders/require'),
+};
+
+Confabulous.processors = {
+  decrypt: require('./lib/processors/decrypt'),
+  envToCamelCaseProp: require('./lib/processors/envToCamelCaseProp'),
+  envToProp: require('./lib/processors/envToProp'),
+  flatten: require('./lib/processors/flatten'),
+  json: require('./lib/processors/json'),
+  mount: require('./lib/processors/mount'),
+  unflatten: require('./lib/processors/unflatten'),
+};
 
 module.exports = Confabulous;
